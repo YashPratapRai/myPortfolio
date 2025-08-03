b4652c510ecb72dbafede03dc1919ae58c17bfc3
raiyashpratap@gmail.com <raiyashpratap@gmail.com>
2025-08-03 11:18:13 +0530

    updated1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -839,7 +839,7 @@ const Home = () => {
                 lineHeight: 1.6
               }}
             >
-              Although I'm not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!
+              Although I'm currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!
             </Typography>
             
             <Button
