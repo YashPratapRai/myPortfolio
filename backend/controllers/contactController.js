d5ac451796b654c77ab17d2ef77afb8477bdbf63
raiyashpratap@gmail.com <raiyashpratap@gmail.com>
2026-02-28 01:43:59 +0530

    Fixed

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -11,7 +11,9 @@ export const submitContactForm = async (req, res) => {
 
     // Email setup
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,        // your Gmail
         pass: process.env.EMAIL_PASS         // App-specific password
